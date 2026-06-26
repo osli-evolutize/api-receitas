24bc6f231127b41722d01ccde62df7dcb4fab33e
Osli de Freyn Marian <osli.marian@evolutize.com.br>
2026-06-26 14:30:52 -0300

    Ajusta caminho padrao do banco no corretor

--- scripts/corrigir-textos-sqlite.js
+++ scripts/corrigir-textos-sqlite.js
@@ -200,8 +200,8 @@ function resolverBanco() {
   const candidatos = [
     caminhoInformado,
     process.env.SQLITE_FILE,
-    path.join(__dirname, "..", "data", "app.db"),
-    path.join(__dirname, "..", "migracao-sqlite", "app.db")
+    path.join(__dirname, "..", "migracao-sqlite", "app.db"),
+    path.join(__dirname, "..", "data", "app.db")
   ].filter(Boolean);
 
   for (const candidato of candidatos) {
